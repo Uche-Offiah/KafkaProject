65052e4c98cc6904e918878d9738795c422d5adb
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-03 22:09:53 +0100

    update ReadMe file

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092", // Kafka broker address
+            ClientId = "dotnet-producer"
+        };
+
+        using var producer = new ProducerBuilder<string, string>(config).Build();
+
+        Console.WriteLine("Enter messages to send (type 'exit' to quit):");
+
+        string topic = "test-topic"; // Kafka topic name
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            if (message.ToLower() == "exit")
+                break;
+
+            var deliveryReport = producer.ProduceAsync(topic, new Message<string, string> { Key = null, Value = message }).Result;
+
+            Console.WriteLine($"Message '{message}' sent to partition {deliveryReport.Partition}, offset {deliveryReport.Offset}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # KafkaProject
 Data streaming with Kafka
+
+--Start ZooKeeper
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+
+--start kafka-server
+.\bin\windows\kafka-server-start.bat .\config\server.properties
+
+-- a consumer and producer resides in the windows folder and there you should navigate with cmd
+kafka-topics.bat --create --bootstrap-server localhost:9092 --topic test-topic
+
+-- Create a producer
+kafka-console-producer.bat --broker-list localhost:9092 --topic test
+
+-- create a consumer
+kafka-console-consumer.bat --topic test --bootstrap-server localhost:9092 --from-beginning
+
+
+--Stop ZooKeeper
+.\bin\windows\zookeeper-server-stop.bat .\config\zookeeper.properties
+
+--Stop Kafka-server
+.\bin\windows\kafka-server-stop.bat .\config\server.properties
+
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test-topic
+
